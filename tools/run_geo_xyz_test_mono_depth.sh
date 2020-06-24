8b71755fa0b13a40d899788100ce4b68fa213fb4
qihao96 <qihao.huang@outlook.com>
2020-06-24 14:01:04 +0800

    fold sh into tools

--- tools/run_geo_xyz_test_mono_depth.sh
+++ tools/run_geo_xyz_test_mono_depth.sh
@@ -2,7 +2,6 @@ export CUDA_VISIBLE_DEVICES=0
 
 user_path="/userhome/34/h3567721"
 
-
 # NOTE: pay attention to test_depth_delta
 python geonet_main.py \
     --mode=test_depth_delta \
@@ -32,8 +31,3 @@ python kitti_eval/eval_depth.py \
     --split=eigen \
     --kitti_dir=$user_path"/dataset/kitti/raw_data/" \
     --pred_file=$user_path"/projects/Depth/GeoNet/predictions/test_xyz_depth_delta_two_stage/model-250000-sn.npy"
-
-python kitti_eval/eval_depth.py \
-    --split=eigen \
-    --kitti_dir=$user_path"/dataset/kitti/raw_data/" \
-    --pred_file=$user_path"/projects/Depth/GeoNet/predictions/model-250000.npy"
